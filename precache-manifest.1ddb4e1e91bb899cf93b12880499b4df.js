82d37b85a3b7f2d4c07dfbced90e47d294a184be
JoseLRiano <joselriano@yahoo.com>
2020-03-30 23:23:39 -0400

    Updates

---
+++ precache-manifest.1ddb4e1e91bb899cf93b12880499b4df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b2723a4ebf0a50d56e5188a2d557a8b",
+    "url": "/ReactPhotoSearchEngine/index.html"
+  },
+  {
+    "revision": "1e1470df1b0df779fc57",
+    "url": "/ReactPhotoSearchEngine/static/css/main.42eacbcb.chunk.css"
+  },
+  {
+    "revision": "4803a25029b11647689e",
+    "url": "/ReactPhotoSearchEngine/static/js/2.c73d4bb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactPhotoSearchEngine/static/js/2.c73d4bb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e1470df1b0df779fc57",
+    "url": "/ReactPhotoSearchEngine/static/js/main.9e8fa377.chunk.js"
+  },
+  {
+    "revision": "9feee821368d7e44c440",
+    "url": "/ReactPhotoSearchEngine/static/js/runtime-main.f83e0996.js"
+  }
+]);
